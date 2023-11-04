6b880ea9542bfd9ce09f93ad3d6df0c7591ed08a
alligator <wavyknight72@gmail.com>
2023-11-04 17:36:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const gridSize = 5;
+const dots = [];
 
+function createGrid() {
+    const gameContainer = document.getElementById("game-container");
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const dot = document.createElement("div");
+            dot.classList.add("dot");
+            dot.setAttribute("data-row", i);
+            dot.setAttribute("data-col", j);
+            dot.addEventListener("click", toggleDot);
+            gameContainer.appendChild(dot);
+            dots.push(dot);
+        }
+    }
+}
+
+function toggleDot() {
+    this.classList.toggle("active");
+
+    // Check for win condition
+    const activeDots = document.querySelectorAll(".dot.active");
+    if (activeDots.length === dots.length) {
+        alert("Congratulations! You connected all the dots!");
+        resetGame();
+    }
+}
+
+function resetGame() {
+    dots.forEach(dot => dot.classList.remove("active"));
+}
+
+createGrid();
